5531afbc10af061049c84403fcbe59fdf59eea21
Ptrag <pjotreg@gmail.com>
2018-08-07 20:26:17 +0200

    finished saving data with NSCoder

---
+++ ListOfTask/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  ListOfTask
+//
+//  Created by Pj on 18/07/2018.
+//  Copyright © 2018 Pj. All rights reserved.
+//
+
+import Foundation
+
+class Item  {
+    
+    var title : String
+    var done : Bool
+}
